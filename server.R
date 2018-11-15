6a43e10e63db53c128e55104cf64c7ca866f4f50
unknown <cerveny.j@czechglobe.cz>
2018-11-15 19:33:08 +0100

    Parameter sets selection ADDED + major cosmetic updates

--- server.R
+++ server.R
@@ -11,53 +11,53 @@ library(shiny)
 
 shinyServer(function(input, output) {
   DIC <- reactive({
-    carb(21, input$pco2, input$pH, input$salinity, input$temp)
+    carb(21, input$pco2, input$pH, S = input$salinity, T = input$temp, k1k2 = input$k1k2)
   })
   output$DIC.forms <- renderUI({
     HTML(paste0(
-      'Dissolved <b>CO<sub>2</sub></b> concentration is <b>',
+      "Dissolved <b>CO<sub>2</sub></b> concentration is <b>",
       round(DIC()$CO2 * 1e6, digits = 1),
-      ' umol/kg</b> [',
+      " &mu;mol/kg</b> [",
       round(DIC()$CO2 / DIC()$DIC * 100, digits = 1),
-      '%, ',
+      "%, ",
       round(DIC()$CO2 * 12.01 * 1000, digits = 1), # 44.01 g/mol
-      ' mg(C)/kg(H<sub>2</sub>O)] <br/>',
-      '<i>Bicarbonate</i> (<b>HCO<sub>3</sub><sup>-</sup></b>) concentration is <b>',
+      " mg(C)/kg(H<sub>2</sub>O)] <br/>",
+      "<i>Bicarbonate</i> (<b>HCO<sub>3</sub><sup>-</sup></b>) concentration is <b>",
       round(DIC()$HCO3 * 1e6, digits = 1),
-      ' umol/kg</b> [',
+      " &mu;mol/kg</b> [",
       round(DIC()$HCO3 / DIC()$DIC * 100, digits = 1),
-      '%, ',
+      "%, ",
       round(DIC()$HCO3 * 12.01 * 1000, digits = 1), # 61.02 g/mol
-      ' mg(C)/kg(H<sub>2</sub>O)] <br/>',
-      '<i>Carbonate</i> (<b>CO<sub>3</sub><sup>2-</sup></b>) concentration is <b>',
+      " mg(C)/kg(H<sub>2</sub>O)] <br/>",
+      "<i>Carbonate</i> (<b>CO<sub>3</sub><sup>2-</sup></b>) concentration is <b>",
       round(DIC()$CO3 * 1e6, digits = 1),
-      ' umol/kg</b> [',
+      " &mu;mol/kg</b> [",
       round(DIC()$CO3 / DIC()$DIC * 100, digits = 1),
-      '%, ',
+      "%, ",
       round(DIC()$CO3 * 12.01 * 1000, digits = 1), # 60.01 g/mol
-      ' mg(C)/kg(H<sub>2</sub>O)] <br/>',
-      'Total dissolved inorganic carbon (<b>DIC</b>) concentration is <b>',
+      " mg(C)/kg(H<sub>2</sub>O)] <br/>",
+      "Total dissolved inorganic carbon (<b>DIC</b>) concentration is <b>",
       round(DIC()$DIC * 1e6, digits = 1),
-      ' umol/kg</b>'
+      " &mu;mol/kg</b>"
     ))
   })
   output$DIC.plot <- renderPlot({
     carb.data <- data.frame(dCO2 = 0, HCO3 = 0, CO3 = 0, DIC = 0)
     pH <- seq(2, 12, 1/3)
-    carb.data <- carb(21, input$pco2, pH, input$salinity, input$temp)[c("CO2","HCO3","CO3","DIC")]
+    carb.data <- carb(21, input$pco2, pH, S = input$salinity, T = input$temp, k1k2 = input$k1k2)[c("CO2","HCO3","CO3","DIC")]
     y <- log10(carb.data$DIC)
     twoord.plot(
       lx = pH,
       ly = carb.data$CO2 / carb.data$DIC,
       rx = pH,
       ry = y,
-      xlab = 'pH',
-      ylab = 'Relative distribution of DIC forms, -',
-      rylab = 'Total DIC, log10(umol/kg)',
+      xlab = "pH",
+      ylab = "Relative distribution of DIC forms, -",
+      rylab = expression("log"[10]*"(Total DIC), "*mu*"mol/kg"),
       rpch = 1
     )
     lines(pH, carb.data$HCO3 / carb.data$DIC, lty = 2)
     lines(pH, carb.data$CO3 / carb.data$DIC, lty = 3)
-    legend(2, 0.6, c("dCO2", "HCO3", "CO3"), lty = c(1, 2, 3))
+    legend(2, 0.6, c(expression("dCO"[2]), expression("HCO"[3]^"-"), expression("CO"[3]^"2-")), lty = c(1, 2, 3))
   })
 })
--- ui.R
+++ ui.R
@@ -17,10 +17,18 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      numericInput('pH','pH', value = 8.1, min = 0, max = 14, step = 0.01),
-      numericInput('pco2','pCO2, ppm', value = 400, min = 0, max = 5000000, step = 1),
-      numericInput('temp','Temperature, oC', value = 25, min = 5, max = 40, step = 0.5),
-      numericInput('salinity','Salinity, g/kg', value = 35, min = 5, max = 44, step = 0.5),
+      numericInput('pH',"pH", value = 8.1, min = 0, max = 14, step = 0.01),
+      numericInput('pco2',"pCO2, ppm", value = 400, min = 0, max = 5000000, step = 1),
+      numericInput('temp',HTML("Temperature, &#8451;"), value = 25, min = 5, max = 40, step = 0.5),
+      numericInput('salinity',"Salinity, g/kg", value = 35, min = 5, max = 44, step = 0.5),
+      selectInput('k1k2',
+                  "Parameter set for k1 and k2:",
+                  c("Lueker et al. (2000)" = "x",
+                    "Millero et al. (2006)" = "m06",
+                    "Millero (2010)" = "m10",
+                    "Waters et al. (2014)" = "w14",
+                    "Roy et al. (1993)" = "r"),
+                  selected = "x"),
       tags$hr(),
       p(
          "@author CzechGlobe - Department of Adaptive Biotechnologies (JaCe)"
